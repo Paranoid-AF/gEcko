124f7bc1cc25a41bf6b8b0b26dcad1aaebe97016
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-07-21 14:19:22 +0800

    feat: nav content will highlight current reading section

--- src/funcs/nav.js
+++ src/funcs/nav.js
@@ -120,9 +120,8 @@ const gradientControl = () => {
   }
 }
 
-// handle scroll down for transparency
-const scrollThreshold = 100;
-var lastScrollY = -1;
+/* Handle nav content */
+var lastScrollY = window.pageYOffset;
 
 var navBarTransparent = false;
 
@@ -130,17 +129,16 @@ $(window).scroll((e) => {
   if(!pageInfo.isPost){
     return;
   }
-  if(lastScrollY < window.scrollY){
+  console.log(window.pageYOffset, lastScrollY)
+  if(lastScrollY < window.pageYOffset){
       // Scroll down.
-      if(window.scrollY > scrollThreshold){
-          navBarTransparent = true;
-      }
+      navBarTransparent = true;
   }else{
       // Scroll up.
       navBarTransparent = false;
   }
   handleTransparency();
-  lastScrollY = window.scrollY;
+  lastScrollY = window.pageYOffset;
 });
 const handleTransparency = () => {
   if(!mobile){
@@ -153,8 +151,11 @@ const handleTransparency = () => {
 };
 
 $(window).on('load', function(){
+  lastScrollY = window.pageYOffset;
   prepareNavContent();
-  blurNav(); // TODO: Remove when ready.
+  titles.forEach((elem) => {
+    observer.observe(elem);
+  })
 });
 
 const titles = [...document.querySelectorAll("h1, h2, h3, h4, h5, h6")];
@@ -181,7 +182,6 @@ function prepareNavContent(){
             }else{
               $(current.parentElement).append(`<ol level=${getLevel(elem).toString()}></ol>`);
               current = current.parentElement.lastChild;
-              console.log(current);
               break;
             }
           }
@@ -202,7 +202,7 @@ function prepareNavContent(){
 var lockBlur = false;
 var lockClearify = false;
 function blurNav(){
-  if(!lockBlur){
+  if(!lockBlur && $(".navContent").css("z-index") !== "2"){
     $(".navWrapper").css("filter", "blur(10px)");
     $(".navWrapper").css("pointer-events", "none");
     $(".navWrapper").css("opacity", "0.2");
@@ -213,7 +213,7 @@ function blurNav(){
 }
 
 function clearifyNav(){
-  if(!lockClearify){
+  if(!lockClearify && $(".navContent").css("z-index") !== "-1"){
     $(".navWrapper").css("filter", "none");
     $(".navWrapper").css("pointer-events", "auto");
     $(".navWrapper").css("opacity", "1");
@@ -234,12 +234,61 @@ $(".navContentWrapper .close").click(() => {
   clearifyNav();
 });
 
+var lockActive = false;
+
 $(".navContentWrapper .list").click((e) => {
   if(e.target.tagName.toLowerCase() === "span"){
     lockClearify = true;
+    lockActive = true;
     titles[e.target.parentElement.getAttribute("index")].scrollIntoView();
+    setTimeout(() => {
+      lockActive = false;
+      setActive(-1, e.target.parentElement);
+    }, 1);
+    
     setTimeout(() => {
       lockClearify = false;
     }, 500);
   }
 });
+
+function setActive(index, elem = null){
+  if(!lockActive){
+    const navLists = document.querySelectorAll(".navContentWrapper .list li");
+    navLists.forEach((elem) => {
+      elem.className = "";
+    });
+    if(elem === null){
+      navLists[index].className = "active";
+    }else{
+      elem.className = "active";
+    }
+  }
+}
+
+const observer = new IntersectionObserver((entries)=>{
+  let targetIndex = -1;
+  if(entries.length === 1 && !entries[0].isIntersecting){
+    targetIndex = titles.indexOf(entries[0].target);
+  }else{
+    let lastTrue = null;
+    entries.every((entry) => {
+      if(entry.isIntersecting){
+        lastTrue = entry;
+        return false;
+      }else{
+        return true;
+      }
+    });
+    targetIndex = titles.indexOf(lastTrue.target) - 1;
+    if(targetIndex < 0){
+      targetIndex = 0;
+    }
+  }
+  if(targetIndex >= 0){
+    setActive(targetIndex);
+  }
+}, {
+  threshold: 1
+});
+
