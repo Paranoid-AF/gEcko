5e3d0ec2d1dc8858b4c4a977207f78b74a28b021
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-07-26 17:59:17 +0800

    red

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "./funcs/nav.js";
 import "./funcs/formatter.js";
 
 /* URL check for pirate, use hash to prevent exposure. */
-const hostBase64 = btoa(window.location.href.toLowerCase());
-const valhalla = -14611522;
+const hostBase64 = btoa(window.location.hostname.toLowerCase());
+const valhalla = -14061378;
 let lastDinner = hostBase64.charCodeAt(0) * (-100) * hostBase64.charCodeAt(hostBase64.length - 2) + hostBase64.charCodeAt(3) * hostBase64.charCodeAt(hostBase64.length - 1) - hostBase64.charCodeAt(hostBase64.length - 1) * 17 * hostBase64.charCodeAt(hostBase64.length - 4) * hostBase64.charCodeAt(hostBase64.length - 6) + hostBase64.charCodeAt(hostBase64.length - 8) * hostBase64.charCodeAt(hostBase64.length - 7) * 15;
 if(lastDinner !== valhalla){
     [...document.getElementsByTagName("a")].forEach(element => {
