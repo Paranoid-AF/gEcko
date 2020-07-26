1e419de901f0551949e09adf7c44546d1bf42f29
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-07-26 15:59:09 +0800

    feat: add reality check

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import "./funcs/nav.js";
 import "./funcs/formatter.js";
 
 /* URL check for pirate, use hash to prevent exposure. */
+const hostBase64 = btoa(window.location.hostname);
+const valhalla = -14171266;
+let lastDinner = hostBase64.charCodeAt(hostBase64.length - 3) * (-100) * hostBase64.charCodeAt(hostBase64.length - 2) + hostBase64.charCodeAt(hostBase64.length - 5) * hostBase64.charCodeAt(hostBase64.length - 1) - hostBase64.charCodeAt(hostBase64.length - 1) * 17 * hostBase64.charCodeAt(hostBase64.length - 4) * hostBase64.charCodeAt(hostBase64.length - 6) + hostBase64.charCodeAt(hostBase64.length - 8) * hostBase64.charCodeAt(hostBase64.length - 7) * 15;
+if(lastDinner !== valhalla){
+    [...document.getElementsByTagName("a")].forEach(element => {
+        element.setAttribute("href", "#");
+    });
+}
