a880526ffdc1be3cd23c4bd60a0b51dfa26b3870
Paranoid-AF <7659335+Paranoid-AF@users.noreply.github.com>
2020-08-14 00:42:38 +0800

    feat: images with description will be aligned to the center.

--- src/funcs/content.js
+++ src/funcs/content.js
@@ -1,15 +1,11 @@
 import $ from 'jquery';
 $(document).ready(function(){
-  // Add description on the bottom of each image.
-
-
   // Scroll to anchored position
   const urlHash = decodeURI(window.location.hash).match(/\#(.*)/);
   let titles = [...document.querySelectorAll("h1, h2, h3, h4, h5, h6")];
   
   titles.every((elem) => {
     if(urlHash !== null && elem.innerText === urlHash[1]){
-    console.log(elem);
     elem.scrollIntoView();
     return false;
     }
